1088e3a8bcef3f4320256af4ce9c6174a10a3af1
vilhelm_fagerstrom <vilhelm.fagerstrom@gu.se>
2022-03-29 13:33:11 +0200

    Bash script used to call lophelia_SVIM_forward_run.py.

---
+++ opendrift/scripts/run_lophelia.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#----------------- Input ------------------
+# Dates for seeding:
+
+ys=2012             # Year/s of particle seeding
+ms=2                # Month/s of particle seeding
+ds=($(seq 1 1 10))  # Day/s of particle seeding
+hs=12               # Hour of particle seeding
+mms=0               # Minute of particle seeding
+
+# Horizontal location for seeding:
+lats=59.545686
+lons=1.537903
+
+# Radius of seeding area [m] (uniform seeding within circular area):
+radis=4000
+
+# Depth for seeding:
+zs=95
+
+# Number of seeding particles at each depth:
+ns=1000
+
+# Length of simulation [days]:
+tsim=5
+
+#--------- Loop over seeding days ---------
+
+#for loop to run script
+for a in "${ds[@]}"
+do
+  python lophelia_SVIM_forward_run_test_1.py $ys $ms "$a" $hs $mms $lats $lons $radis $zs $ns $tsim
+  echo "Simulation for seeding on day $a done"
+done
